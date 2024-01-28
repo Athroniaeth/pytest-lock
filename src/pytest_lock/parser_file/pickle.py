6c49d59ed3971d4d5183998ad9bc2daec60b23d9
Athroniaeth <pierre.chaumont@hotmail.fr>
2024-01-29 00:59:44 +0100

    Add Pickle parser

---
+++ src/pytest_lock/parser_file/pickle.py
@@ -0,0 +1,54 @@
+import json
+import pickle
+import sys
+from pathlib import Path
+
+from pytest_lock.models.cache.file import FileCache
+from pytest_lock.models.cache.lock import Lock
+from pytest_lock.parser_file.base import ParserFile
+
+if sys.version_info >= (3, 12):
+    from typing import override
+else:
+    from typing_extensions import override
+
+
+class ParserFilePickle(ParserFile):
+    """
+    Parser for Pickle file
+
+    Notes:
+        This parser provides better quality locks than json, which is limited by its
+        requirement for "SupportsStr" objects. It's important to be aware that it's
+        dangerous to use the Pickle extension, as it can execute arbitrary code,
+        even if theoretically there shouldn't be a problem as long as you
+        don't use external Pickle objects.
+
+    Attributes:
+        encoding: Encoding of file
+        protocol: Protocol of pickle file (default: pickle.HIGHEST_PROTOCOL)
+    """
+
+    def __init__(self, encoding: str = "ASCII", protocol: int = pickle.HIGHEST_PROTOCOL):
+        super().__init__(encoding=encoding)
+        self.protocol = protocol
+
+    @override
+    def read_file(self, path: Path) -> FileCache:
+        if not path.exists():
+            return FileCache(functions=[])
+
+        content = pickle.load(open(f'{path}', 'rb'), encoding=self.encoding)
+
+        if not isinstance(content, FileCache):
+            raise TypeError(f"FileCache expected, got {type(content)}")
+
+        return content
+
+    @override
+    def write_file(self, path: Path, file_cache: FileCache) -> None:
+        if not path.exists():
+            path.parent.mkdir(parents=True, exist_ok=True)
+            path.touch()
+
+        return pickle.dump(file_cache, open(f'{path}', 'wb'), protocol=pickle.HIGHEST_PROTOCOL)
