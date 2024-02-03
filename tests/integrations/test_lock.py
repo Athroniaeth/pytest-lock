89ee1a108d7607fe941edf914add7f24e216ee87
Athroniaeth <pierre.chaumont@hotmail.fr>
2024-02-03 18:01:46 +0100

    Add diversified tests (extension) for reinforce test

--- tests/integrations/test_lock.py
+++ tests/integrations/test_lock.py
@@ -3,11 +3,12 @@ from _pytest.pytester import Pytester
 from pytest_lock.models.cli_argument import ArgumentCLI
 from pytest_lock.parser_file.builder import ParserFileBuilder
 
+number_extension = ParserFileBuilder().mapping.keys().__len__()
+number_tests = 8 * number_extension  # Each test is repeated for each extension
+
 
 def test_lock_diversified(pytester: Pytester) -> None:
     """Test scenario representing a "classic" pytest_lock test which will be used in Pytester"""
-    number_extension = ParserFileBuilder().mapping.keys().__len__()
-    number_tests = 8 * number_extension  # Each test is repeated for each extension
 
     pytester.copy_example("conftest.py")
     old_test_path = pytester.copy_example("scenarios/test_fixture_lock_diversified.py")
--- tests/integrations/test_lock_change_date.py
+++ tests/integrations/test_lock_change_date.py
@@ -3,6 +3,10 @@ import datetime
 from _pytest.pytester import Pytester
 
 from pytest_lock.models.cli_argument import ArgumentCLI
+from pytest_lock.parser_file.builder import ParserFileBuilder
+
+number_extension = ParserFileBuilder().mapping.keys().__len__()
+number_tests = 8 * number_extension  # Each test is repeated for each extension
 
 
 def test_lock_change_date_today(pytester: Pytester) -> None:
@@ -11,13 +15,13 @@ def test_lock_change_date_today(pytester: Pytester) -> None:
     today = today.strftime("%Y/%m/%d")
 
     pytester.copy_example("conftest.py")
-    pytester.copy_example("scenarios/test_fixture_lock.py")
+    pytester.copy_example("scenarios/test_fixture_lock_diversified.py")
 
     result = pytester.runpytest(ArgumentCLI.LOCK, ArgumentCLI.LOCK_DATE, f"{today}")
-    result.assert_outcomes(passed=1)
+    result.assert_outcomes(passed=number_tests)
 
     result = pytester.runpytest()
-    result.assert_outcomes(passed=1)
+    result.assert_outcomes(passed=number_tests)
 
 
 def test_lock_change_date_yesterday(pytester: Pytester) -> None:
@@ -26,13 +30,13 @@ def test_lock_change_date_yesterday(pytester: Pytester) -> None:
     yesterday = yesterday.strftime("%Y/%m/%d")
 
     pytester.copy_example("conftest.py")
-    pytester.copy_example("scenarios/test_fixture_lock.py")
+    pytester.copy_example("scenarios/test_fixture_lock_diversified.py")
 
     result = pytester.runpytest(ArgumentCLI.LOCK, ArgumentCLI.LOCK_DATE, f"{yesterday}")
-    result.assert_outcomes(passed=1)
+    result.assert_outcomes(passed=number_tests)
 
     result = pytester.runpytest()
-    result.assert_outcomes(failed=1)
+    result.assert_outcomes(failed=number_tests)
 
 
 def test_lock_change_date_tomorrow(pytester: Pytester) -> None:
@@ -41,10 +45,10 @@ def test_lock_change_date_tomorrow(pytester: Pytester) -> None:
     tomorrow = tomorrow.strftime("%Y/%m/%d")
 
     pytester.copy_example("conftest.py")
-    pytester.copy_example("scenarios/test_fixture_lock.py")
+    pytester.copy_example("scenarios/test_fixture_lock_diversified.py")
 
     result = pytester.runpytest(ArgumentCLI.LOCK, ArgumentCLI.LOCK_DATE, f"{tomorrow}")
-    result.assert_outcomes(passed=1)
+    result.assert_outcomes(passed=number_tests)
 
     result = pytester.runpytest()
-    result.assert_outcomes(passed=1)
+    result.assert_outcomes(passed=number_tests)
